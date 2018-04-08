b5619c66603ddd54691e2fee913fad2ab8d1f55c
Denis Mosolov <denismosolov@gmail.com>
2018-04-08 10:56:12 +0300

    Print success message when chroot environment is done

--- bootstrap/11-pm.sh
+++ bootstrap/11-pm.sh
@@ -113,5 +113,7 @@ if ${CREATE_ONLY_CHROOT}; then
 
     cleanup
 
+    success "Chroot environment ${R} is done. Now you can speed up next builds by passing it to Pieman via the BASE_DIR parameter."
+
     exit 0
 fi
