03e290d1daa93d3c26114978d68a80e90968c4e3
Evgeny Golyshev <eugulixes@gmail.com>
2018-05-12 20:41:02 +0300

    Do not reload systemd manager confirmation at startup
    The code was inherited from rpi23-gen-image. On the one hand, there is no need to reload systemd manager configuration (if so, let one of the scripts from files/firstboot do it explicitly) and the set of commands seem to be redundant. On the other hand, some of the supported distributions (Devuan, for example) don't have systemd, so it's wrong to call systemctl without any check.

--- files/firstboot/99-finish.sh
+++ files/firstboot/99-finish.sh
@@ -1,7 +1,3 @@
-logger -t "rc.firstboot" "Reload systemd manager configuration"
-systemctl daemon-reload
-systemctl restart networking.service
-systemctl restart systemd-networkd.service
 logger -t "rc.firstboot" "First boot actions finished"
 rm -f /etc/rc.firstboot
 sed -i '/.*rc.firstboot/d' /etc/rc.local
