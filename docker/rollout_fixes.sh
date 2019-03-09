6785d9fd9b70bcf7126685f86ae42899abb1acac
Evgeny Golyshev <eugulixes@gmail.com>
2019-03-09 23:39:37 +0300

    Update dependencies in Dockerfile

--- docker/rollout_fixes.sh
+++ docker/rollout_fixes.sh
@@ -2,15 +2,33 @@
 
 set -e
 
-mkdir qemu-user-static
+mkdir packages
 
-cd qemu-user-static
+#
+# qemu-arm-static
+#
 
 wget http://ftp.debian.org/debian/dists/stretch/main/binary-amd64/Packages.xz
 
 xz -d Packages.xz
 
-package=$(grep "Filename: pool/main/q/qemu/qemu-user-static" Packages | awk '{print $2}')
+cd packages
+
+package=$(grep "Filename: pool/main/q/qemu/qemu-user-static" ../Packages | awk '{print $2}')
+
+wget http://ftp.debian.org/debian/${package}
+
+ar x $(basename ${package})
+
+tar xJvf data.tar.xz
+
+#
+# zlib1g
+#
+
+# There are three packages: zlib1g, zlib1g-dbg and zlib1g-dev. Use -m1 to get
+# the first one.
+package=$(grep -m1 "Filename: pool/main/z/zlib/zlib1g" ../Packages | awk '{print $2}')
 
 wget http://ftp.debian.org/debian/${package}
 
@@ -19,8 +37,12 @@ ar x $(basename ${package})
 tar xJvf data.tar.xz
 
 cp usr/bin/qemu-arm-static /usr/bin
-
 cp usr/bin/qemu-aarch64-static /usr/bin
+cp lib/x86_64-linux-gnu/libz.so.1.2.8 /usr/glibc-compat/lib/libz.so.1
 
 cd ..
+
+# cleanup
+rm    Packages
+rm -r packages
 
