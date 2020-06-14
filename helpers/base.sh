fc8001731a56724a408e9bb758af20254a7d9bf9
Evgeny Golyshev <eugulixes@gmail.com>
2020-06-14 23:24:29 +0300

    Update copyright year in helpers/base.sh

--- helpers/base.sh
+++ helpers/base.sh
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 Evgeny Golyshev <eugulixes@gmail.com>
+# Copyright (C) 2018-2020 Evgeny Golyshev <eugulixes@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
