8fae0e8434dfd5cfc2d34bb5dfb574342c0b550c
d.budarina <82271384+db-yaya@users.noreply.github.com>
2021-05-14 10:28:48 +0000

    Update misc.sh

--- helpers/misc.sh
+++ helpers/misc.sh
@@ -1,4 +1,5 @@
-# Copyright (C) 2018 Evgeny Golyshev <eugulixes@gmail.com>
+# Copyright (C) 2019 Denis Gavrilyuk <karpa4o4@gmail.com>
+# Copyright (C) 2018-2020 Evgeny Golyshev <eugulixes@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -12,22 +13,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# Checks if Pieman is run in the docker container based on its official Docker
-# image cusdeb/pieman.
-# Globals:
-#     None
-# Arguments:
-#     None
-# Returns:
-#     Boolean
-check_if_run_in_docker() {
-    if [ -f /.dockerenv ]; then
-        true
-    else
-        false
-    fi
-}
 
 # Adds the specified package name to the BASE_PACKAGES environment variable
 # which is a comma-separated list.
@@ -69,6 +54,23 @@ add_option_to_pm_options() {
     add_item_to_list "${1}" PM_OPTIONS " "
 }
 
+# Checks if Pieman is run in the docker container based on its official Docker
+# image cusdeb/pieman.
+# Globals:
+#     None
+# Arguments:
+#     None
+# Returns:
+#     Boolean
+check_if_run_in_docker() {
+    if [ -f /.dockerenv ]; then
+        true
+    else
+        false
+    fi
+}
+
+
 # Creates a keyring from the public keys related to the operating system which
 # is going to be used as a base for the target image. Then, the keyring is
 # passed to debootstrap. The keyring name is stored in the KEYRING environment
@@ -84,6 +86,29 @@ create_keyring() {
     for key in keys/"${PIECES[0]}"/*; do
         gpg --no-default-keyring --keyring="${KEYRING}" --import "${key}"
     done
+}
+
+# Encrypts the specified password using the SHA-512 method.
+# Globals:
+#     PYTHON
+# Arguments:
+#     Password
+# Returns:
+#     Encrypted password
+do_mkpasswd() {
+    "${PYTHON}" -c "import crypt; print(crypt.crypt('$1', crypt.mksalt(crypt.METHOD_SHA512)))"
+}
+
+# Downloads files from the Web non-interactively.
+# Globals:
+#     None
+# Arguments:
+#     wget options (only -O and -q are supported)
+#     URL
+# Returns:
+#     None
+do_wget() {
+    wget.py "$@"
 }
 
 # Adds the public keys, related to the operating system which is used as a base
