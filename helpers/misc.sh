724b23f3e263646d88960f0478354402392cbab7
d.budarina <82271384+db-yaya@users.noreply.github.com>
2021-05-14 10:54:48 +0000

    Update misc.sh

--- helpers/misc.sh
+++ helpers/misc.sh
@@ -14,6 +14,80 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# Adds the specified package name to the BASE_PACKAGES environment variable
+# which is a comma-separated list.
+# Globals:
+#     BASE_PACKAGES
+# Arguments:
+#     Package name
+# Returns:
+#     None
+add_package_to_base_packages() {
+    if is_alpine; then
+        add_item_to_list "${1}" BASE_PACKAGES " "
+    else
+        add_item_to_list "${1}" BASE_PACKAGES ","
+    fi
+}
+
+# Adds the specified package name to the INCLUDES environment variable which is
+# a comma-separated list.
+# Globals:
+#     INCLUDES
+# Arguments:
+#     Package name
+# Returns:
+#     None
+add_package_to_includes() {
+    add_item_to_list "${1}" INCLUDES ","
+}
+
+# Adds the specified options to the PM_OPTIONS environment variable which is
+# a space-separated list.
+# Globals:
+#     PM_OPTIONS
+# Arguments:
+#     option
+# Returns:
+#     None
+add_option_to_pm_options() {
+    add_item_to_list "${1}" PM_OPTIONS " "
+}
+
+# Checks if Pieman is run in the docker container based on its official Docker
+# image cusdeb/pieman.
+# Globals:
+#     None
+# Arguments:
+#     None
+# Returns:
+#     Boolean
+check_if_run_in_docker() {
+    if [ -f /.dockerenv ]; then
+        true
+    else
+        false
+    fi
+}
+
+
+# Creates a keyring from the public keys related to the operating system which
+# is going to be used as a base for the target image. Then, the keyring is
+# passed to debootstrap. The keyring name is stored in the KEYRING environment
+# variable.
+# Globals:
+#     PIECES
+#     KEYRING
+# Arguments:
+#     None
+# Returns:
+#     None
+create_keyring() {
+    for key in keys/"${PIECES[0]}"/*; do
+        gpg --no-default-keyring --keyring="${KEYRING}" --import "${key}"
+    done
+}
+
 # Encrypts the specified password using the SHA-512 method.
 # Globals:
 #     PYTHON
@@ -37,3 +111,26 @@ do_wget() {
     wget.py "$@"
 }
 
+# Adds the public keys, related to the operating system which is used as a base
+# for the target image, to the list of trusted keys.
+# Globals:
+#     PIECES
+#     R
+# Arguments:
+#     None
+# Returns:
+#     None
+mark_keys_as_trusted() {
+    for key in keys/"${PIECES[0]}"/*; do
+        local key_name=""
+
+        key_name=$(basename "${key}")
+
+        cp "${key}" "${R}"
+
+        info "adding ${key} to the list of trusted keys"
+        chroot_exec apt-key add "${key_name}"
+
+        chroot_exec rm "${key_name}"
+    done
+}
